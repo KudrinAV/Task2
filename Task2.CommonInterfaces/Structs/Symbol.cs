722242195a91c0a155614a3ff8b711eb09908297
Антон Кудрин <antonkudrin25@gmail.com>
2017-11-06 18:53:39 +0300

    Added  struct Symbol. And Interfaces for Words, Punction Mars and Sentences.

---
+++ Task2.CommonInterfaces/Structs/Symbol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.CommonInterfaces.Structs
+{
+    public struct Symbol
+    {
+        Char[] Chars;
+
+        public Symbol (string chars)
+        {
+            Chars = chars.ToCharArray();
+        }
+    }
+}
