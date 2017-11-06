71ab9238708b3e9274e4c5cc310f2f1b41fc62b3
Антон Кудрин <antonkudrin25@gmail.com>
2017-11-06 19:09:56 +0300

    Added ISentence interface.

---
+++ Task2.CommonInterfaces/Interfaces/ISentence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.CommonInterfaces.Interfaces
+{
+    public interface ISentence
+    {
+        List<ISentenceElement> Sentence { get; }
+        int NumberOfWords { get; }
+
+        void ReplaceWordForSubstring(int length);
+    }
+}
