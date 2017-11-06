65d768e65a5b9fb11b901b6bcb792cab76954a27
Антон Кудрин <antonkudrin25@gmail.com>
2017-11-06 20:20:44 +0300

    Added class Sentence

---
+++ Task2.Common/TextObjects/Sentence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.CommonInterfaces.Interfaces;
+
+namespace Task2.Common.TextObjects
+{
+    class Sentence : ISentence
+    {
+        public int NumberOfWords => throw new NotImplementedException();
+
+        public bool IsInterrogativeSentence => throw new NotImplementedException();
+
+        List<ISentenceElement> ISentence.Sentence => throw new NotImplementedException();
+
+        public void ReplaceWordForSubstring(int length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
