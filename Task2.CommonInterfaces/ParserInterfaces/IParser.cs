52c20a8a6cba728a053361cb26734b64597d8847
Антон Кудрин <antonkudrin25@gmail.com>
2017-11-06 20:28:07 +0300

    Added IParser interface.

---
+++ Task2.CommonInterfaces/ParserInterfaces/IParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.CommonInterfaces.Interfaces;
+
+namespace Task2.CommonInterfaces.ParserInterfaces
+{
+    interface IParser
+    {
+        IText TextParser(StreamReader streamReader);
+        ISentence SentenceParser(string sentence);
+
+    }
+}
