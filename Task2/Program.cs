84f5136ed6c5507ac54194e10cb4c90498e3b56e
Антон Кудрин <antonkudrin25@gmail.com>
2017-11-09 23:49:45 +0300

    Remaked Task2.CommonClasses from consApp to ClassLibrary

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task2.CommonClasses.ParserObj;
 using Task2.CommonInterfaces.Interfaces;
 
 namespace Task2
@@ -11,7 +13,21 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+        
+                Parser myParser = new Parser();
+                StreamReader streamReader = new StreamReader(@"..\test.txt");
+                IText text = new CommonClasses.TextObjects.Text();
+                text = myParser.TextParser(streamReader);
+                foreach (var item in text.Sentences)
+                {
+                    foreach (var temp in item.Elements)
+                    {
+                        Console.Write(temp.Value + "  ");
+                    }
+                    Console.WriteLine();
+                }
             
         }
+    
     }
 }
