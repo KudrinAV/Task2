090ba3b66437bd877f791fe3695a3a78c6bf6a58
Антон Кудрин <antonkudrin25@gmail.com>
2017-11-13 20:56:02 +0300

    Changed method for more clarity

--- Task2.Common/TextObjects/Text.cs
+++ Task2.Common/TextObjects/Text.cs
@@ -15,10 +15,8 @@ namespace Task2.CommonClasses.TextObjects
         public IEnumerable<string> FindWordsInInterrogativeSentences(int length)
         {
             var finding = from sentence in Sentences
-                          group sentence by sentence.IsInterrogativeSentence() into interrogativeSentences
-                          where interrogativeSentences.Key
-                          from elements in interrogativeSentences
-                          from element in elements.Elements
+                          where sentence.IsInterrogativeSentence()  
+                          from element in sentence.Elements
                           group element by element is IWord && element.Value.Length == length into wordGroup
                           where wordGroup.Key
                           from word in wordGroup
